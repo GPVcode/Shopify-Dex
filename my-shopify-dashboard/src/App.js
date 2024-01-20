bdfe8d6fa29a1eeadeef074ced3235cb1529ca2d
GPV <gvill005@gmail.com>
2024-01-19 19:56:34 -0800

    working on app plan

--- my-shopify-dashboard/src/App.js
+++ my-shopify-dashboard/src/App.js
@@ -2,7 +2,8 @@ function App() {
   return (
     <h1>Welcome to Shopify Dex</h1>
     <p>Shopify Dex is a comprehensive dedicated tool suite for your growing Shopify business</p>
-  );
+    <p>Currently working on application API plan.</p>
+    );
 }
 
 export default App;
